ab6b605472d4d020da4550c9ed0504d09368135b
Bogdan Martyniuk <bmartyniuk@MacBook-Pro-Bogdan.local>
2025-02-16 16:32:14 +0100

    Update main.yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './e2e',  
-  retries: 2,
-  use: {
-    headless: true,
-    viewport: { width: 1280, height: 720 },
-    baseURL: 'https://username.github.io/repository-name/',
-    ignoreHTTPSErrors: true,
-    screenshot: 'only-on-failure',
-    trace: 'on-first-retry',
-  },
   reporter: [
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
     ['json', { outputFile: 'playwright-report/test-results.json' }] // Додано JSON-звіт!
   ],
+  use: {
+    trace: 'on',
+  },
 });
